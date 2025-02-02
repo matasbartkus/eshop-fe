b211ebcf603a3787ee61586cf4fa3c411a16dc57
Matas Bartkus <matbar1@ktu.lt>
2025-02-02 18:44:20 +0200

    Modified navbar logic, added favicon

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,77 +11,93 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
-import ShoppingBag  from '@mui/icons-material/ShoppingBagOutlined';
 import navBarItems from './NavBarItems/NavBarItems';
 import { Drawer } from '@mui/material';
 import { Link } from 'react-router-dom';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 
-interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window?: () => Window;
-}
+const NavBar = () =>{
 
-const NavBar = (props: Props) => {
-  const { window } = props;
-  const drawerWidth = 240;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const handleDrawerToggle = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  }
 
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-  };
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle}
-      sx={{textAlign: "center"}}
-      >
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        Watches
-      </Typography>
-      <Divider></Divider>
-      <List>
-        {navBarItems.Buttons.map(({ label, link }) => (
-          <ListItem key={label} disablePadding>
-            <ListItemButton href={link} sx={{textAlign: "center"}}>
-              <ListItemText primary={label} ></ListItemText>
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+  const DrawerList = (
+    <Box
+      sx={{ width: 240, textAlign: "center" }} onClick={handleDrawerToggle(false)}
+      role="presentation">
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          modWatches
+       </Typography>
+       <Divider></Divider>
+       <List>
+         {navBarItems.Buttons.map(({ label, link }) => (
+           <ListItem key={label} disablePadding>
+             <ListItemButton href={link} sx={{textAlign: "center"}}>
+               <ListItemText primary={label} ></ListItemText>
+             </ListItemButton>
+           </ListItem>
+         ))}
+       </List>
     </Box>
-  );
-  
-  const container = window !== undefined ? () => window().document.body : undefined;
-
+  )
   return (
-    <Box sx={{ flexGrow: 1, display: "flex"
-     }}>
+    <Box sx={{flexGrow: 1}}>
       <AppBar sx={{
         bgcolor: "#242424"
-      }} position="static">
+      }}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2,  }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link
-            to={navBarItems.Home.link}
-            style={{textDecoration:"none", color:"inherit"}}
-            >
-              {navBarItems.Home.label}
-            </Link>
+        <Typography 
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{mr: 2, display: {xs: "none", md: "flex"}}}>
+              <Link 
+                to={navBarItems.Home.link} 
+                style={{ textDecoration: "none", color: "inherit" }}>
+                  {navBarItems.Home.label}
+              </Link>
           </Typography>
+          <Box 
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleDrawerToggle(true)}
+              sx={{ mr: 2,  }}
+            >
+              <MenuIcon />
+              <Drawer open={open} 
+                onClose={handleDrawerToggle(false)}
+                variant="temporary"
+                >
+                {DrawerList}
+              </Drawer>
+            </IconButton>
+          </Box>
+          <Typography 
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{flexGrow: 1, display: {xs: "flex", md: "none"}}}>
+              <Link 
+                to={navBarItems.Home.link} 
+                style={{ textDecoration: "none", color: "inherit" }}>
+                  {navBarItems.Home.label}
+              </Link>
+          </Typography>
+          <Box sx={{flexGrow: 1}} />
+          <Box sx={{display: {xs: "none", md: "flex"}}}>
+            {navBarItems.Buttons.map(({label, link}) => (
+              <Button color="inherit" component={Link} to={link}>
+                {label}
+              </Button>
+            ))}
+          </Box>
           <IconButton
           color="inherit"
           size="large"
@@ -94,9 +110,9 @@ const NavBar = (props: Props) => {
             </Link>
           </IconButton>
           <IconButton
-          color="inherit"
-          size="large"
-          sx={{mr: 1}}
+            color="inherit"
+            size="large"
+            sx={{mr: 1}}
           >
             <Link to={navBarItems.Instagram.link} 
             style={{textDecoration:"none", color:"inherit"}}>
@@ -104,27 +120,10 @@ const NavBar = (props: Props) => {
               </InstagramIcon>
             </Link>
           </IconButton>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
-      <nav>
-        <Drawer 
-        container={container}
-        variant="temporary"
-        open={mobileOpen}
-        onClose={handleDrawerToggle}
-        ModalProps={{
-          keepMounted: true,
-        }}
-        sx={{
-          display: { xs: 'block', },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-        }}
-        >
-          {drawer}
-        </Drawer>
-      </nav>
     </Box>
   );
 }
+  
 export default NavBar;
--- src/components/Products/ProductCards.tsx
+++ src/components/Products/ProductCards.tsx
@@ -6,8 +6,8 @@ import products from '../../data/ProductsData';
 
 export default function ProductsCards(){
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={{ xs: 2, sm: 8, md: 1 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        <Box sx={{ flexGrow: 1, flexDirection: "column", justifyContent: "center" }}>
+            <Grid container spacing={{ xs: 2, sm: 8, md: 1 }} columns={{ xs: 4, sm: 8, md: 12 }} justifyContent="center">
             {products.map((product) => (
                 <Grid key={product.name} size={{ xs: 2, sm: 4, md: 4 }}>
                     <Card image={product.images[0]} name={product.name}></Card>
